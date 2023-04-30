dd828242d6e2796851c2c3b8bbc592c69da719a2
Bill Zhang <billzhangsc@gmail.com>
2023-04-30 01:31:34 -0700

    Initial

--- server/main.py
+++ server/main.py
@@ -4,13 +4,10 @@ import base64
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

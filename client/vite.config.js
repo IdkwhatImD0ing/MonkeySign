3d166fa0e35261db768f2bcf97ff77869112e467
Simon Quach <43255108+simon-quach@users.noreply.github.com>
2023-04-30 13:35:58 -0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,4 +15,9 @@ export default defineConfig({
   build: {
     target: "esnext", //browsers can handle the latest ES features
   },
+  esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  }
 });
